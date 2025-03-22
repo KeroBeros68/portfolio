19c15691988adeb3bfdb10f964b46f1fe1844145
KeroBeros68 <kevinbertrand68.pro@gmail.com>
2025-03-22 08:08:28 +0100

    navbar mobil update

--- Frontend/src/components/navbar/navbar.jsx
+++ Frontend/src/components/navbar/navbar.jsx
@@ -21,6 +21,13 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet'
 
+import {
+  Accordion,
+  AccordionItem,
+  AccordionContent,
+  AccordionTrigger,
+} from '../ui/accordion'
+
 import logo from '@/assets/images/logo.png'
 import { Separator } from '@radix-ui/react-separator'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -29,7 +36,7 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 const linkStyle =
   ' text-lg bg-[linear-gradient(to_right,var(--background),var(--background))] hover:bg-[linear-gradient(var(--background),var(--emerald-800))]'
 
-const menuNav = (
+const menuDesktop = (
   <NavigationMenu>
     <NavigationMenuList className="flex flex-col md:flex-row">
       <NavigationMenuItem>
@@ -89,6 +96,52 @@ const menuNav = (
   </NavigationMenu>
 )
 
+const menuMobil = (
+  <Sheet>
+    <SheetTrigger>
+      <Menu className="h-8 w-8" />
+    </SheetTrigger>
+    <SheetContent className="items-center">
+      <SheetHeader className="w-full items-center">
+        <img
+          src={logo}
+          alt="logo de Kévin BERTRAND - Développeur web"
+          className="mt-10 w-30"
+        />
+      </SheetHeader>
+      <Separator className="mx-2 mt-1 h-px w-[60%] bg-[var(--border)]" />
+      <Link to={'/'}>Acceuil</Link>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1" className="text-center">
+          <AccordionTrigger className="text-base font-normal">
+            A propos
+          </AccordionTrigger>
+          <AccordionContent>
+            <ul className="flex flex-col gap-3">
+              <li>
+                <Link to={'/kevinbertrand'}>Présentation</Link>
+              </li>
+              <li>
+                <Link to={'/skills'}>Skills</Link>
+              </li>
+              <li>
+                <Link to={'/experiences'}>Expériences</Link>
+              </li>
+              <li>
+                <Link to={'/formation'}>Formation</Link>
+              </li>
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <Link to={'/portfolio'}>Portfolio</Link>
+      <Link to={'/contact'}>Contact</Link>
+      <Separator className="mx-2 mt-1 h-px w-[60%] bg-[var(--border)]" />
+      {mediaNav}
+    </SheetContent>
+  </Sheet>
+)
+
 const mediaNav = (
   <div className="ml-4 flex items-center gap-3">
     <a href="">
@@ -105,7 +158,7 @@ function Navbar() {
     <>
       <nav>
         <div className="hidden md:flex">
-          {menuNav}
+          {menuDesktop}
           <Separator
             orientation="vertical"
             className="mx-2 mt-1 h-7 w-px bg-[var(--border)]"
@@ -113,24 +166,7 @@ function Navbar() {
           {mediaNav}
         </div>
         <div className="m-0.5 flex items-center justify-between md:hidden">
-          <Sheet>
-            <SheetTrigger>
-              <Menu className="h-8 w-8" />
-            </SheetTrigger>
-            <SheetContent className="items-center">
-              <SheetHeader className="w-full items-center">
-                <img
-                  src={logo}
-                  alt="logo de Kévin BERTRAND - Développeur web"
-                  className="mt-10 w-30"
-                />
-              </SheetHeader>
-              <Separator className="mx-2 mt-1 h-px w-[60%] bg-[var(--border)]" />
-              <SheetDescription>{menuNav}</SheetDescription>
-              <Separator className="mx-2 mt-1 h-px w-[60%] bg-[var(--border)]" />
-              {mediaNav}
-            </SheetContent>
-          </Sheet>
+          {menuMobil}
         </div>
       </nav>
     </>
